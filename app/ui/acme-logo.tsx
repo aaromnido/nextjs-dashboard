3c7e595031403c8af583f8b9c10caf74b335c4c6
Fernando <fernando@inspiration-q.com>
2024-10-03 19:03:35 +0200

    Fixing fonts

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,10 +1,10 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { poppins } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${poppins.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-8 w-8 rotate-[15deg]" />
       <p className="text-[18px] ml-1">Inspiration-Q</p>
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,13 +1,8 @@
-import { Inter, Poppins, Lusitana } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-
-export const poppins = Poppins({ 
-    subsets: ['latin'],
-    weight: ['400', '700']
-});
-
+ 
 export const lusitana = Lusitana({
+  weight: ['400', '700'],
   subsets: ['latin'],
-  weight: ['400', '700']
 });
