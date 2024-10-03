73e12d28df720a907e8ca5686c74aadb86317301
Fernando <fernando@inspiration-q.com>
2024-10-03 18:48:23 +0200

    Adding Lusitana to the fonts file

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,13 @@
-import { Inter, Poppins } from 'next/font/google';
+import { Inter, Poppins, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
+
 export const poppins = Poppins({ 
     subsets: ['latin'],
     weight: ['400', '700']
-  });
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700']
+});
